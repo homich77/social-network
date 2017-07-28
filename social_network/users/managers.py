c97dc5aa02ef709b016cca53afcf0f599c739da6
Vlad Bezpalko <vl.bezpalko@gmail.com>
2017-07-28 09:34:21 +0300

    Move UserManager to managers.py

---
+++ social_network/users/managers.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+from users.tasks import fill_user_data
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        fill_user_data.delay(user.pk)
+        return user
+
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields['is_staff'] = True
+        extra_fields['is_superuser'] = True
+
+        return self._create_user(email, password, **extra_fields)
--- social_network/users/models.py
+++ social_network/users/models.py
@@ -1,33 +1,9 @@
 import clearbit
-from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
-from users.tasks import fill_user_data
-
-
-class UserManager(BaseUserManager):
-    use_in_migrations = True
-
-    def _create_user(self, email, password, **extra_fields):
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-
-        fill_user_data.delay(user.pk)
-        return user
-
-    def create_user(self, email=None, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_superuser', False)
-        return self._create_user(email, password, **extra_fields)
-
-    def create_superuser(self, email, password, **extra_fields):
-        extra_fields['is_staff'] = True
-        extra_fields['is_superuser'] = True
-
-        return self._create_user(email, password, **extra_fields)
+from users.managers import UserManager
 
 
 class User(AbstractBaseUser, PermissionsMixin):
